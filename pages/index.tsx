b48466b3e249a12cadf01f904120994ce38f0608
hassan214241 <hassan214241@gmail.com>
2021-08-23 03:49:23 -0700

    changes new

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,57 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css';
 import 'antd/dist/antd.css';
+import { Form, Input, Button, Checkbox } from 'antd';
 
-import "../styles/Style.css";
+// import "../styles/Style.css";
 
 
 const Home: NextPage = () => {
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+  
   return (
- <div  className="main">
+    <div style={{justifyContent:'center',alignItems:'center', height:"full",width:"full", backgroundColor:"green",flex:"center"}}>
+    <Form
+    name="basic"
+    labelCol={{ span: 8 }}
+    wrapperCol={{ span: 16 }}
+    initialValues={{ remember: true }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+  >
+    <Form.Item
+      label="Username"
+      name="username"
+      rules={[{ required: true, message: 'Please input your username!' }]}
+    >
+      <Input />
+    </Form.Item>
 
-   HAssan
+    <Form.Item
+      label="Password"
+      name="password"
+      rules={[{ required: true, message: 'Please input your password!' }]}
+    >
+      <Input.Password />
+    </Form.Item>
 
- </div>
+    <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+      <Checkbox>Remember me</Checkbox>
+    </Form.Item>
+
+    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form>
+  </div>
   )
 }
 
