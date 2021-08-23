6a912b0ba36c048fd38b9c7e44b59a96d16c5815
Mubashar639 <hmubashar639@gmail.com>
2021-08-23 15:36:15 +0500

    change made and debug

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css';
-import 'antd/dist/antd.css';
+import '../styles/Home.module.css';
+// import "../styles/Style.css";
 
-import "../styles/Style.css";
 
 
 const Home: NextPage = () => {
   return (
- <div  className="main">
+    <div className="main">
 
-   HAssan
+      HAssan
 
- </div>
+    </div>
   )
 }
 
